2ba9921aeee21cee15f0a910c1194202ca40d762
jagaru <satguanersuhtaokirosnne_622@yahoo.co.jp>
2017-01-19 14:17:20 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+#coding:utf8
+import random
+
+def is_prime3(q, k):
+    q = abs(q)
+    #計算するまでもなく判定できるものははじく
+    if q == 2: return True
+    if q < 2 or q&1 == 0: return False
+
+    #n-1=2^s*dとし（但しaは整数、dは奇数)、dを求める
+    d = (q-1)>>1
+    while d&1 == 0:
+        d >>= 1
+    
+    #判定をk回繰り返す
+    for i in xrange(k):
+        a = random.randint(1, q-1)
+        t = d
+        y = pow(a, t, q)
+        #[0,s-1]の範囲すべてをチェック
+        while t != q-1 and y != 1 and y != q-1: 
+            y = pow(y, 2, q)
+            t <<= 1
+        if y != q-1 and t&1 == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    n = 12345
+    l = [int(x) for x in list(str(n))]
+    is_prime3(l[2], 50);
+
